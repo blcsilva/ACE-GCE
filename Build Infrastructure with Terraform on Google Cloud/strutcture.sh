3fa0a683e6512a4996e54a1417ffe68463d26911
Bruno Lucio <50206342+blcsilva@users.noreply.github.com>
2024-04-10 03:01:48 -0300

    Create strutcture.sh
    Creating directory structure and files:

---
+++ Build Infrastructure with Terraform on Google Cloud/strutcture.sh
@@ -0,0 +1,16 @@
+touch main.tf
+touch variables.tf
+mkdir modules
+cd modules
+mkdir instances
+cd instances
+touch instances.tf
+touch outputs.tf
+touch variables.tf
+cd ..
+mkdir storage
+cd storage
+touch storage.tf
+touch outputs.tf
+touch variables.tf
+cd
