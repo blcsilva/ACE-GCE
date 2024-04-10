ecbd87abc8006378d96b61b0d27600f3bf2fb2a7
Bruno Lucio <50206342+blcsilva@users.noreply.github.com>
2024-04-10 03:20:19 -0300

    Configure Backend and define future steps

---
+++ Build Infrastructure with Terraform on Google Cloud/Task 3 - Configure a remote backend/backend_config.sh
@@ -0,0 +1,24 @@
+cat > main.tf <<EOF
+terraform {
+  backend "gcs" {
+    # Configuration for remote backend
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.53.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+module "instances" {
+  source     = "./modules/instances"
+}
+EOF
+echo "yes" | terraform init
