f5410e8e3fea26252a46ff74a397ac582f49ddc9
Bruno Lucio <50206342+blcsilva@users.noreply.github.com>
2024-04-10 03:03:30 -0300

    Create config_main.sh
    Configuring main Terraform file (main.tf):

---
+++ Build Infrastructure with Terraform on Google Cloud/config_main.sh
@@ -0,0 +1,20 @@
+cat > main.tf <<EOF
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.53.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+module "instances" {
+  source     = "./modules/instances"
+}
+EOF
